069dfd600647d23a7939df0f5e66aca7b04fef6e
BradT <BradT@10.250.4.45>
2017-01-18 13:24:03 -0400

    Initial Submission

---
+++ src/w2017week2/W2017week2.java
@@ -0,0 +1,17 @@
+
+package w2017week2;
+
+/**
+ *
+ * @author BradT
+ */
+public class W2017week2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub world!!");
+    }
+    
+}
